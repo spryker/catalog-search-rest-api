a25eedbaec4ee88c69f5cfbea83b1fb5bd90e4c9
René Klatt <rene.klatt@spryker.com>
2026-06-24 10:11:26 +0200

    Updated doc blocks (#581)
    * Updated doc blocks

--- src/Spryker/Glue/CatalogSearchRestApi/CatalogSearchRestApiConfig.php
+++ src/Spryker/Glue/CatalogSearchRestApi/CatalogSearchRestApiConfig.php
@@ -12,26 +12,36 @@ use Spryker\Glue\Kernel\AbstractBundleConfig;
 class CatalogSearchRestApiConfig extends AbstractBundleConfig
 {
     /**
+     * @api
+     *
      * @var string
      */
     public const RESOURCE_CATALOG_SEARCH = 'catalog-search';
 
     /**
+     * @api
+     *
      * @var string
      */
     public const RESOURCE_CATALOG_SEARCH_SUGGESTIONS = 'catalog-search-suggestions';
 
     /**
+     * @api
+     *
      * @var string
      */
     public const QUERY_STRING_PARAMETER = 'q';
 
     /**
+     * @api
+     *
      * @var string
      */
     public const RESPONSE_CODE_PARAMETER_MUST_BE_INTEGER = '503';
 
     /**
+     * @api
+     *
      * @var string
      */
     public const ERROR_MESSAGE_PARAMETER_MUST_BE_INTEGER = 'Value of %s must be of type integer.';
